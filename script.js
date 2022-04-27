47d00f3468be795b3fb4cca336b27e8fa2465268
Travis <travis.m.bruce@gmail.com>
2022-04-27 11:25:03 -0700

    Finalize password input error message

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let submitButton = document.querySelector("#form-submit");
+let submitButton = document.querySelector("#submit-btn");
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirm-password");
+let passField = document.querySelector("#pass-field");
 
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
+confirmPassword.addEventListener("input", (e) => {
+  if (password.value != confirmPassword.value) {
+    passField.classList.add("input-error");
+  } else {
+    passField.classList.remove("input-error");
+  }
 });
